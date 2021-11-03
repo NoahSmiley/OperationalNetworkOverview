5079b25cbd6fd3c3a59d8d14b74e5bc972d0686b
Noah Smiley <noahsmiley123@gmail.com>
2021-11-03 00:03:58 -0500

    Testing Arrow Nav Functionality

--- Node_React_APP/frontend/src/App.js
+++ Node_React_APP/frontend/src/App.js
@@ -1,22 +1,51 @@
 import "./App.css";
+import { useRef } from "react";
+import { useEffect } from "react";
 import Navigation from "./Components/UI/Navigation";
 import CentralReport from "./Components/Central Report/CentralReport";
-import ImageSlideShow from './Components/Slideshow/ImageSlideShow';
-import MapSlideShow from './Components/Slideshow/MapSlideShow';
+import ImageSlideShow from "./Components/Slideshow/ImageSlideShow";
+import MapSlideShow from "./Components/Slideshow/MapSlideShow";
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
+function useKey(key, cb) {
+  const callbackRef = useRef(cb);
+
+  useEffect(() => {
+    callbackRef.current = cb;
+  });
+  useEffect(() => {
+    function handle(event) {
+      if (event.code === key) {
+        callbackRef.current(event);
+      }
+    }
+    document.addEventListener("keydown", handle);
+    return () => document.removeEventListener("keydown", handle);
+  }, [key]);
+}
+
 function App() {
+  function handleArrowLeft() {
+    console.log("Left");
+  }
+  function handleArrowRight() {
+    console.log("Right");
+  }
+  useKey("ArrowLeft", handleArrowLeft);
+  useKey("ArrowRight", handleArrowRight);
+
   return (
     <div>
-        {/* Switch Controles React Router - and links the navbar to components */}
-        <Switch>
-            <Route exact path="/central-report" component={CentralReport} />
-            <Route path="/images" component={ImageSlideShow} />
-            <Route path="/maps" component={MapSlideShow} />
-        </Switch>
+      {/* Switch Controles React Router - and links the navbar to components */}
 
-        {/* NavBar */}
-        <Navigation/>
+      <Switch>
+        <Route exact path="/central-report" component={CentralReport} />
+        <Route path="/images" component={ImageSlideShow} />
+        <Route path="/maps" component={MapSlideShow} />
+      </Switch>
+
+      {/* NavBar */}
+      <Navigation />
     </div>
   );
 }
